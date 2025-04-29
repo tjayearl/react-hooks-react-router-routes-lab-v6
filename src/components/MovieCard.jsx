47cd83adda3b80baba1ed4374381746b33c79108
tjayearl <iamtjayearl@gmail.com>
2025-04-29 15:37:04 +0300

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,11 @@
-// src/components/MovieCard.jsx
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function MovieCard({ id, title }) {
+function MovieCard({ movie }) {
   return (
-    <article>
-      <h2>{title}</h2>
-      {/* Ensure the path matches your routes.jsx definition */}
-      <Link to={`/movies/${id}`}>View Details</Link>
-    </article>
+    <div>
+      <h2>{movie.title}</h2>
+      <Link to={`/movie/${movie.id}`}>View Info</Link>
+    </div>
   );
 }
 
