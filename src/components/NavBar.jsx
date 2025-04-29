c8c845f80f25eddf7f9a5badb4a2fc10d44dadc1
tjayearl <iamtjayearl@gmail.com>
2025-04-29 15:39:00 +0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,12 @@
-// src/components/NavBar.jsx
 import { NavLink } from "react-router-dom";
-import "./NavBar.css"; // Make sure this CSS file exists and styles appropriately
 
 function NavBar() {
-  // Helper function for active class styling
-  const getNavLinkClass = ({ isActive }) => isActive ? "active" : undefined;
-
   return (
-    <nav className="navbar">
-      {/* Use function in className prop for v6 compatibility */}
-      <NavLink to="/" className={getNavLinkClass}>Home</NavLink>
-      <NavLink to="/actors" className={getNavLinkClass}>Actors</NavLink>
-      <NavLink to="/directors" className={getNavLinkClass}>Directors</NavLink>
-    </nav>
+    <div className="navbar" role="navigation">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
   );
 }
 
