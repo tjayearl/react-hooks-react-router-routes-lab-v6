8531bcf01df16cca836f96539ce419c709faa30e
tjayearl <iamtjayearl@gmail.com>
2025-04-29 15:48:43 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-// src/main.jsx
-import "./index.css"; // Ensure this file exists
-import React from 'react'; // Import React
+
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "./routes"; 
 
 const router = createBrowserRouter(routes);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// Wrap with StrictMode for development checks
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
