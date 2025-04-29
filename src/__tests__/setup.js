9a5415ca3f8eff9ef96172d0d65a25b40460ee5c
tjayearl <iamtjayearl@gmail.com>
2025-04-29 15:35:20 +0300

    Update setup.js

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,11 +1,9 @@
-// src/__tests__/setup.js
-import { afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
-
-// Remove the Blob import from 'node:buffer' - jsdom provides it globally.
-// Remove 'whatwg-fetch' - jsdom provides fetch globally.
+import { afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+import 'whatwg-fetch';
+import {Blob} from 'node:buffer';
 
 afterEach(() => {
     cleanup();
-});
+})
